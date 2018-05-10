3037bd9a5b4118f4129ccdc371135457e512b4c8
[lutianqi] <[1179555783@qq.com]>
2018-05-10 15:38:37 +0800

    新增

---
+++ src/main/java/com/soa/ProxyDemo/RealSubject.java
@@ -0,0 +1,11 @@
+package com.soa.ProxyDemo;
+
+public class RealSubject implements Subject {
+    public void rent() {
+        System.out.println("I want to rent my house");
+    }
+
+    public void hello(String str) {
+        System.out.println("hello: " + str);
+    }
+}
---
+++ src/main/java/com/soa/ProxyDemo/Subject.java
@@ -0,0 +1,8 @@
+package com.soa.ProxyDemo;
+
+public interface Subject {
+
+    public void rent();
+
+    public void hello(String str);
+}
